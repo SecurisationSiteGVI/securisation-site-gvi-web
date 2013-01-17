a300467295bbff0a358f598073c08abe963d8f2d
damienChesneau <ches.damien@gmail.com>
2013-01-17 15:41:19 +0100

    2

---
+++ src/java/client/NewClass.java
@@ -0,0 +1,14 @@
+package client;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author damien
+ */
+public class NewClass {
+    
+}
