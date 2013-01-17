dd3690e25825e766be96bfbba847790703e0b651
damienChesneau <ches.damien@gmail.com>
2013-01-17 17:24:48 +0100

    create utilisateurManagedBean

---
+++ src/java/client/UtilisateurManagedBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author damien
+ */
+@ManagedBean(name = "utilisateurManagedBean1")
+@RequestScoped
+public class UtilisateurManagedBean {
+
+    /**
+     * Creates a new instance of UtilisateurManagedBean
+     */
+    public UtilisateurManagedBean() {
+    }
+}
