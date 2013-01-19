78675ad2fb3be1d6c7dbdfb9e0687dd2afe28a48
damienChesneau <ches.damien@gmail.com>
2013-01-19 10:17:42 +0100

    add CSSPath

---
+++ src/java/client/CSSPath.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author damien
+ */
+@ManagedBean(name="cssPath")
+public class CSSPath {
+    private String protocol ="http://";
+    private String host ="localhost:8080";
+    private String applicationName = "securisation-site-gvi-web";
+    private String repertoire ="css";
+    private String path = this.protocol+this.host+"/"+this.applicationName+"/"+this.repertoire;
+    
+    public String getCssStyle(){
+        String style = this.path+"/style.css";
+        return style;
+    }
+    public String getCssStyle1(){
+        String style = this.path+"/style1.css";
+        return style;
+    }
+    public String getCssStylebMin(){
+        String style = this.path+"/styleb-min.css";
+        return style;
+    }
+    public String getCssStyle1Min(){
+        String style = this.path+"/style1-min.css";
+        return style;
+    }
+    public String getCssStyleb(){
+        String style = this.path+"/styleb.css";
+        return style;
+    }
+    public String getCssDocs(){
+        String style = this.path+"/docs.css";
+        return style;
+    }
+}
