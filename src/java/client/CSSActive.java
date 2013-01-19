142a837b48014c6df1f12d7f7929aec341395606
damienChesneau <ches.damien@gmail.com>
2013-01-19 11:04:36 +0100

    add propertiers

--- src/java/client/CSSActive.java
+++ src/java/client/CSSActive.java
@@ -20,14 +20,25 @@ public class CSSActive {
     private String url;
     private String pageTechnicien = " ";
     private String histrorique=" ";
+    private String utilisateur =" ";
+    private String listeUtilisateur = " ";
+    private String ajouterUtilisateurs =" ";
     public CSSActive() {
         FacesContext context = FacesContext.getCurrentInstance();
         this.url = context.getViewRoot().getViewId();
         this.url = url.substring(1);
+        System.out.println(this.url);
         if("pageTechnicien.xhtml".equals(url)){
             this.pageTechnicien = "active";
         }if("histrorique.xhtml".equals(url)){
             this.histrorique = "active";
+        }if("utilisateurs/ajouterUtilisateurs.xhtml".equals(url)){
+            this.utilisateur = "active";
+            this.ajouterUtilisateurs="active";
+        }
+        if("utilisateurs/listeUtilisateur.xhtml".equals(url)){
+            this.utilisateur = "active";
+            this.listeUtilisateur="active";
         }
        
     }
@@ -54,5 +65,29 @@ public class CSSActive {
 
     public void setHistrorique(String histrorique) {
         this.histrorique = histrorique;
+    }
+
+    public String getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(String utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public String getListeUtilisateur() {
+        return listeUtilisateur;
+    }
+
+    public void setListeUtilisateur(String listeUtilisateur) {
+        this.listeUtilisateur = listeUtilisateur;
+    }
+
+    public String getAjouterUtilisateurs() {
+        return ajouterUtilisateurs;
+    }
+
+    public void setAjouterUtilisateurs(String ajouterUtilisateurs) {
+        this.ajouterUtilisateurs = ajouterUtilisateurs;
     }
 }
