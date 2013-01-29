675dad7daa1a45acaf223fc07f32b7b199464f54
damienChesneau <ches.damien@gmail.com>
2013-01-29 17:04:33 +0100

    add addposiontionManagedBean

---
+++ src/java/client/position/AddPositionManagedBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client.position;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author damien
+ */
+@ManagedBean
+@RequestScoped
+public class AddPositionManagedBean {
+
+    public AddPositionManagedBean() {
+    }
+}
