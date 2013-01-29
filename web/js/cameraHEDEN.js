45542733158253db9d7310c8ed35e0795614916d
MarvinBlondelle <marvin.blondelle@wanadoo.fr>
2013-01-29 17:42:09 +0100

    ajout Js cameraHEDEN

---
+++ web/js/cameraHEDEN.js
@@ -0,0 +1,65 @@
+var http; // Notre objet XMLHttpRequest
+
+function createRequestObject()
+{
+    var http;
+    if (window.XMLHttpRequest)
+    { // Mozilla, Safari, IE7 ...
+        http = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject)
+    { // Internet Explorer 6
+        http = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return http;
+}
+
+function haut()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://172.16.79.214/decoder_control.cgi?user=admin&pwd=marvin&command=0&onestep=1', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+}
+
+function bas()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://172.16.79.214/decoder_control.cgi?user=admin&pwd=marvin&command=2&onestep=1', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+}
+
+function gauche()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://172.16.79.214/decoder_control.cgi?user=admin&pwd=marvin&command=4&onestep=1', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+}
+
+function droite()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://172.16.79.214/decoder_control.cgi?user=admin&pwd=marvin&command=6&onestep=1', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+}
+
+function handleAJAXReturn()
+{
+    //alert('handleAJAXReturn');
+    if (http.readyState == 4)
+    {
+        if (http.status == 200)
+        {
+            document.getElementById('resultat').innerHTML = http.responseText;
+        }
+
+    }
+}
+
