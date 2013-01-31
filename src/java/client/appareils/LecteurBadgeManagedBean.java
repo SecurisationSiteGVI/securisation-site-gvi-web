83af8c312ab6d8a8de4cad2c11369f0b54b61605
damienChesneau <ches.damien@gmail.com>
2013-01-31 12:38:17 +0100

    add getPosition method

--- src/java/client/appareils/LecteurBadgeManagedBean.java
+++ src/java/client/appareils/LecteurBadgeManagedBean.java
@@ -4,11 +4,17 @@
  */
 package client.appareils;
 
+import client.BoiteAOutils;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import metier.BorneAccesService;
 import metier.MetierFactory;
+import metier.PositionService;
 import metier.entitys.BorneAcces;
+import metier.entitys.Position;
 
 /**
  *
@@ -17,7 +23,7 @@ import metier.entitys.BorneAcces;
 @ManagedBean
 @RequestScoped
 public class LecteurBadgeManagedBean {
-
+    //succesLecteurBadge 
     private BorneAccesService borneAccesSrv = MetierFactory.getBorneAccesService();
     private BorneAcces borneAcces = new BorneAcces();
     public LecteurBadgeManagedBean() {
@@ -30,6 +36,17 @@ public class LecteurBadgeManagedBean {
         return borneAcces;
     }
 
+    public List<Position> getPositions(){
+        PositionService positionSrv = MetierFactory.getPositionService();
+        List<Position> positions = null;
+        try {
+            positions = positionSrv.getAll();
+        } catch (Exception ex) {
+            BoiteAOutils.addMessage("Erreur", " impossible de récupéré la liste des positions", "errorLecteurBadge");
+            Logger.getLogger(LecteurBadgeManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return positions;
+    }
     public void setBorneAcces(BorneAcces borneAcces) {
         this.borneAcces = borneAcces;
     }
