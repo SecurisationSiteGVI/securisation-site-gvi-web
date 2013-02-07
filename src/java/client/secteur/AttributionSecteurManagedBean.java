82bfabb14bd3650264294e7ac176c7868294bd01
damienChesneau <ches.damien@gmail.com>
2013-02-07 16:42:29 +0100

    add fuction to add borneacces

--- src/java/client/secteur/AttributionSecteurManagedBean.java
+++ src/java/client/secteur/AttributionSecteurManagedBean.java
@@ -10,6 +10,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ValueChangeEvent;
 import metier.AttributionSecteurBorneAccesService;
 import metier.AttributionSecteurCameraService;
 import metier.AttributionSecteurDetecteurIntrusionService;
@@ -31,18 +33,20 @@ import metier.entitys.Secteur;
  * @author damien
  */
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class AttributionSecteurManagedBean {
 
-    private SecteurService secteurSrv=  MetierFactory.getSecteurService();
+    private SecteurService secteurSrv = MetierFactory.getSecteurService();
     private BorneAccesService borneAccesSrv = MetierFactory.getBorneAccesService();
     private DetecteurIntrusionService detecteurIntrusionSrv = MetierFactory.getDetecteurIntrusionService();
-    private CameraService cameraSrv =MetierFactory.getCameraService();
+    private CameraService cameraSrv = MetierFactory.getCameraService();
     private AttributionSecteurBorneAccesService attributionSecteurBorneAccesSrv = MetierFactory.getAttributionSecteurBorneAccesService();
     private AttributionSecteurCameraService attributionSecteurCameraSrv = MetierFactory.getAttributionSecteurCameraService();
     private AttributionSecteurDetecteurIntrusionService attributionSecteurDetecteurIntrusionSrv = MetierFactory.getAttributionSecteurDetecteurIntrusionService();
     private Secteur secteurSelected;
-    public List<Secteur> getSecteurs(){
+    private BorneAcces borneAccesSelected;
+
+    public List<Secteur> getSecteurs() {
         List<Secteur> secteurs = null;
         try {
             secteurs = this.secteurSrv.getAll();
@@ -51,10 +55,17 @@ public class AttributionSecteurManagedBean {
         }
         return secteurs;
     }
-    public void attribuerCamera(){
-        
+
+    public void attribuerBorne() {
+        this.attributionSecteurBorneAccesSrv.attribuerBorneAcces(this.secteurSelected, this.borneAccesSelected);
     }
-    public List<Camera>  getCameras() {
+
+    public void setDynamicSecteurSelected(ValueChangeEvent evt) {
+        Secteur s = (Secteur) evt.getNewValue();
+        this.secteurSelected = s;
+    }
+
+    public List<Camera> getCameras() {
         List<Camera> cameras = null;
         try {
             cameras = this.cameraSrv.getAll();
@@ -63,7 +74,8 @@ public class AttributionSecteurManagedBean {
         }
         return cameras;
     }
-    public List<BorneAcces> getBorneAcceses(){
+
+    public List<BorneAcces> getBorneAcceses() {
         List<BorneAcces> borneAcceses = null;
         try {
             borneAcceses = this.borneAccesSrv.getAll();
@@ -72,7 +84,8 @@ public class AttributionSecteurManagedBean {
         }
         return borneAcceses;
     }
-    public List<DetecteurIntrusion> getDetecteurIntrusions(){
+
+    public List<DetecteurIntrusion> getDetecteurIntrusions() {
         List<DetecteurIntrusion> detecteurIntrusions = null;
         try {
             detecteurIntrusions = this.detecteurIntrusionSrv.getAll();
@@ -81,32 +94,57 @@ public class AttributionSecteurManagedBean {
         }
         return detecteurIntrusions;
     }
-    public List<Object> getAppareilsBySecteur(){
+
+    public List<Object> getAppareilsBySecteur() {
         List<Object> objects = new ArrayList<Object>();
-        if(this.secteurSelected !=null){
-           AttributionSecteurBorneAcces attributionSecteurBorneAcceses = this.attributionSecteurBorneAccesSrv.getBySecteur(this.secteurSelected);
-           List<BorneAcces> borneAcceses = attributionSecteurBorneAcceses.getBorneAccess();
-           if(!borneAcceses.isEmpty()){
-               objects.addAll(borneAcceses);
-           }
-           AttributionSecteurCamera attributionSecteurCamera = this.attributionSecteurCameraSrv.getBySecteur(this.secteurSelected);
-           List<Camera> cameras = attributionSecteurCamera.getCameras();
-           if(!cameras.isEmpty()){
-               objects.addAll(cameras);
-           }
-           AttributionSecteurDetecteurIntrusion attributionSecteurDetecteurIntrusion = this.attributionSecteurDetecteurIntrusionSrv.getBySecteur(secteurSelected);
-           List<DetecteurIntrusion> detecteurIntrusions = attributionSecteurDetecteurIntrusion.getDetecteurIntrusions();
-           if(!detecteurIntrusions.isEmpty()){
-               objects.addAll(detecteurIntrusions);
-           }
+        if (this.secteurSelected != null) {
+            AttributionSecteurBorneAcces attributionSecteurBorneAcceses = this.attributionSecteurBorneAccesSrv.getBySecteur(this.secteurSelected);
+            if (attributionSecteurBorneAcceses != null) {
+                List<BorneAcces> borneAcceses = attributionSecteurBorneAcceses.getBorneAccess();
+                if (!borneAcceses.isEmpty()) {
+                    objects.addAll(borneAcceses);
+                }
+            }
+            AttributionSecteurCamera attributionSecteurCamera = this.attributionSecteurCameraSrv.getBySecteur(this.secteurSelected);
+            if (attributionSecteurCamera != null) {
+                List<Camera> cameras = attributionSecteurCamera.getCameras();
+                if (!cameras.isEmpty()) {
+                    objects.addAll(cameras);
+                }
+            }
+            AttributionSecteurDetecteurIntrusion attributionSecteurDetecteurIntrusion = this.attributionSecteurDetecteurIntrusionSrv.getBySecteur(secteurSelected);
+            if (attributionSecteurDetecteurIntrusion != null) {
+                List<DetecteurIntrusion> detecteurIntrusions = attributionSecteurDetecteurIntrusion.getDetecteurIntrusions();
+                if (!detecteurIntrusions.isEmpty()) {
+                    objects.addAll(detecteurIntrusions);
+                }
+            }
         }
         return objects;
     }
+
     public Secteur getSecteurSelected() {
+        Secteur secteur = null;
+        if(this.secteurSelected==null){
+            try {
+                secteur = this.secteurSrv.getAll().get(0);
+                this.secteurSelected = secteur;
+            } catch (Exception ex) {
+                Logger.getLogger(AttributionSecteurManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         return secteurSelected;
     }
 
     public void setSecteurSelected(Secteur secteurSelected) {
         this.secteurSelected = secteurSelected;
+    }
+
+    public BorneAcces getBorneAccesSelected() {
+        return borneAccesSelected;
+    }
+
+    public void setBorneAccesSelected(BorneAcces borneAccesSelected) {
+        this.borneAccesSelected = borneAccesSelected;
     }
 }
