923f2aff5058d13f61f1d3086cd428f9973004b1
damienChesneau <ches.damien@gmail.com>
2013-02-28 16:45:55 +0100

    add method for have details

--- src/java/client/HistoriqueManagedBean.java
+++ src/java/client/HistoriqueManagedBean.java
@@ -4,12 +4,20 @@
  */
 package client;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 import metier.EvenementService;
 import metier.MetierFactory;
+import metier.entitys.Acces;
 import metier.entitys.Evenement;
+import metier.entitys.Intrusion;
+import metier.entitys.Photo;
 
 /**
  *
@@ -22,10 +30,39 @@ public class HistoriqueManagedBean {
     private List<Evenement> evenements;
     private EvenementService evenementSrv = MetierFactory.getEvenementService();
     private int index;
-    private int nbResult =10;
+    private int nbResult = 10;
+    private Object objectSelected;
+    private Acces accesView;
+    private Intrusion intrusionView;
+    private Photo photoView;
+
     public HistoriqueManagedBean() {
-        this.index =0;
+        this.index = 0;
         this.evenements = evenementSrv.getAll(this.index, this.nbResult);
+    }
+
+    public void more() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+        session.setAttribute("historique", this.objectSelected);
+        if (objectSelected instanceof Photo) {
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().redirect(LinksPath.getPathLinkStatic() + "/historique/photo.jsf");
+            } catch (IOException ex) {
+                Logger.getLogger(HistoriqueManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (objectSelected instanceof Acces) {
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().redirect(LinksPath.getPathLinkStatic() + "/historique/acces.jsf");
+            } catch (IOException ex) {
+                Logger.getLogger(HistoriqueManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (objectSelected instanceof Intrusion) {
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().redirect(LinksPath.getPathLinkStatic() + "/historique/intrusion.jsf");
+            } catch (IOException ex) {
+                Logger.getLogger(HistoriqueManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     public List<Evenement> getEvenements() {
@@ -34,5 +71,63 @@ public class HistoriqueManagedBean {
 
     public void setEvenements(List<Evenement> evenements) {
         this.evenements = evenements;
+    }
+
+    public Object getObjectSelected() {
+        return objectSelected;
+    }
+
+    public void setObjectSelected(Object objectSelected) {
+        this.objectSelected = objectSelected;
+    }
+
+    public void retour() {
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect(LinksPath.getPathLinkStatic() + "/histrorique.jsf");
+        } catch (IOException ex) {
+            Logger.getLogger(HistoriqueManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Acces getAccesView() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        Acces s = null;
+        if (session.getAttribute("historique") != null) {
+            s = (Acces) session.getAttribute("historique");
+            this.accesView = s;
+        }
+        return accesView;
+    }
+
+    public void setAccesView(Acces accesView) {
+        this.accesView = accesView;
+    }
+
+    public Intrusion getIntrusionView() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        Intrusion s = null;
+        if (session.getAttribute("historique") != null) {
+            s = (Intrusion) session.getAttribute("historique");
+            this.intrusionView = s;
+        }
+        return intrusionView;
+    }
+
+    public void setIntrusionView(Intrusion intrusionView) {
+        this.intrusionView = intrusionView;
+    }
+
+    public Photo getPhotoView() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        Photo s = null;
+        if (session.getAttribute("historique") != null) {
+            s = (Photo) session.getAttribute("historique");
+            this.photoView = s;
+        }
+        return photoView;
+    }
+
+    public void setPhotoView(Photo photoView) {
+        this.photoView = photoView;
     }
 }
