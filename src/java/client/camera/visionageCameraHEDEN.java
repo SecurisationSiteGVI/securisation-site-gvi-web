60adbf063e1ecfba60a7a3dfe1acb63b66de89eb
MarvinBlondelle <marvin.blondelle@wanadoo.fr>
2013-03-08 12:12:18 +0100

    photo

--- src/java/client/camera/visionageCameraHEDEN.java
+++ src/java/client/camera/visionageCameraHEDEN.java
@@ -5,6 +5,12 @@ package client.camera;
  * and open the template in the editor.
  */
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
@@ -12,7 +18,13 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
 import javax.servlet.http.HttpSession;
+import metier.EvenementService;
+import metier.MetierFactory;
 import metier.entitys.Camera;
+import metier.entitys.Photo;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import physique.io.CameraDriver;
 
 /**
@@ -45,6 +57,51 @@ public class visionageCameraHEDEN {
             session.setAttribute("camera", c.getIp());
     }
 
+        public void photo() throws FileNotFoundException, IOException{
+        
+        
+        
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        FileOutputStream fos = new FileOutputStream("/home/blondellemarvin/Bureau/photo.jpg");
+        
+        DefaultHttpClient client2 = new DefaultHttpClient();
+        HttpGet request2 = new HttpGet("http://172.16.79.214/snapshot.jpg?user=admin&pwd=marvin");
+        HttpResponse response2 = client2.execute(request2);
+
+        BufferedInputStream is = new BufferedInputStream(response2.getEntity().getContent());
+
+        byte[] buff = new byte[10240];
+        int n;
+        int total = 0;
+        while ((n = is.read(buff)) != -1) {
+            if( n >= buff.length) {
+                n = buff.length-1;
+            }
+            
+            bos.write(buff, 0, n);
+            fos.write(buff, 0, n);
+ 
+            total += n;
+            
+        }
+        System.out.println("IMG : " + total);
+        fos.close();
+        bos.close();
+        is.close();
+        //response.getWriter().print("ok");
+        
+        Photo photo = new Photo();
+        photo.setDateEvt(new Date());
+        photo.setImage(buff);
+
+        EvenementService es= MetierFactory.getEvenementService();
+        try {
+            es.add(photo);
+            System.out.println("envoye");
+        } catch (Exception ex) {
+            Logger.getLogger(Capture.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
     /**
 
     /**
