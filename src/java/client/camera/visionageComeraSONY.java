ace35b81e50c53d635897431470e8834fe4f439d
MarvinBlondelle <marvin.blondelle@wanadoo.fr>
2013-03-15 09:14:10 +0100

    ajout new class

---
+++ src/java/client/camera/visionageComeraSONY.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client.camera;
+
+/**
+ *
+ * @author marvin
+ */
+public class visionageComeraSONY {
+    
+}
