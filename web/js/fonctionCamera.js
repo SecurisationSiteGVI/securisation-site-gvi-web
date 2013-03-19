d36cedaa5b65411cd51cd0d7044a280623a78081
MarvinBlondelle <marvin.blondelle@wanadoo.fr>
2013-03-19 11:17:22 +0100

    rename

---
+++ web/js/fonctionCamera.js
@@ -0,0 +1,90 @@
+var http; // Notre objet XMLHttpRequest
+
+function createRequestObject()
+{
+    var http;
+    if (window.XMLHttpRequest)
+    { // Mozilla, Safari, IE7 ...
+        http = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject)
+    { // Internet Explorer 6
+        http = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return http;
+}
+
+
+
+function haut()
+{
+    
+    http = createRequestObject();
+    
+    http.open('POST', 'http://'+ip+'/decoder_control.cgi?user=admin&pwd=marvin&command=0&onestep=1', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+    
+}
+
+function bas()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://'+ip+'/decoder_control.cgi?user=admin&pwd=marvin&command=2&onestep=1', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+}
+
+function gauche()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://'+ip+'/decoder_control.cgi?user=admin&pwd=marvin&command=4&onestep=1', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+}
+
+function droite()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://'+ip+'/decoder_control.cgi?user=admin&pwd=marvin&command=6&onestep=1', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+}
+
+function auto()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://'+ip+'/decoder_control.cgi?user=admin&pwd=marvin&command=28', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+
+}
+
+function stop()
+{
+    http = createRequestObject();
+    
+    http.open('POST', 'http://'+ip+'/decoder_control.cgi?user=admin&pwd=marvin&command=29', true);
+    http.onreadystatechange = handleAJAXReturn;
+    http.send(null);
+
+}
+
+
+
+function handleAJAXReturn()
+{
+    //alert('handleAJAXReturn');
+    if (http.readyState == 4)
+    {
+        if (http.status == 200)
+        {
+            document.getElementById('resultat').innerHTML = http.responseText;
+        }
+    }
+}
+
